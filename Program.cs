4b12757404a3fafb9d8858afb688eee6615a09af
DrReiz1 <d.rodrigues.reis1@gmail.com>
2026-09-12 10:23:41 -0300

    Allow user to input numbers

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Program;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=========CALCULATOR=========");
+        Console.WriteLine("Insert the first number");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Insert the second number");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+    }
+}
