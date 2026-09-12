2d009b066f6b8e47700af040c45772ff3dd6f31c
DrReiz1 <d.rodrigues.reis1@gmail.com>
2026-09-12 10:38:46 -0300

    Adding the Switch pattern for mathematical operations

--- Program.cs
+++ Program.cs
@@ -21,5 +21,36 @@ class Program
         Console.WriteLine("0. Exit");
         int option = Convert.ToInt32(Console.ReadLine());
 
+        switch (option)
+        {
+            case 0:
+
+            break;
+
+            case 1:
+
+            break;
+
+            case 2:
+
+            break;
+
+            case 3:
+
+            break;
+
+            case 4:
+
+            break;
+
+            case 5:
+
+            break;
+
+            case 6:
+
+            break; 
+        }
+
     }
 }
