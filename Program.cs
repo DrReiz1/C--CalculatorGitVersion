30ab1863d55b606ee25b8371821c05f15936cb98
DrReiz1 <d.rodrigues.reis1@gmail.com>
2026-09-12 10:35:57 -0300

    Logical Operation Menu for the User

--- Program.cs
+++ Program.cs
@@ -10,5 +10,16 @@ class Program
 
         Console.WriteLine("Insert the second number");
         int number2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Select the logical operation you want to execute:");
+        Console.WriteLine("1. Addition");
+        Console.WriteLine("2. Subtraction");
+        Console.WriteLine("3. Multiplication");
+        Console.WriteLine("4. Division");
+        Console.WriteLine("5. Remainder");
+        Console.WriteLine("6. Exponentiation");
+        Console.WriteLine("0. Exit");
+        int option = Convert.ToInt32(Console.ReadLine());
+
     }
 }
